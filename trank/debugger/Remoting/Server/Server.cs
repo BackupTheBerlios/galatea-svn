4381f0450266aac9347974a3eb0aba45f10a074b
pavlov <pavlov@75ff9695-dde4-0310-ae9e-a7f1fc817e45>
2004-10-16 20:43:33 +0000

    remoting initial layout

---
+++ trank/debugger/Remoting/Server/Server.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using TypesLibrary;
+
+namespace Server
+{
+	/// <remarks>
+	/// Sample server to demonstrate the use of .NET Remoting.
+	/// </remarks>
+	public class SampleServer
+	{
+		public static int Main()
+		{
+			// Create an instance of a channel
+			TcpChannel channel = new TcpChannel(8080);
+			ChannelServices.RegisterChannel(channel);
+
+			// Register as an available service with the name HelloWorld
+			RemotingConfiguration.RegisterWellKnownServiceType(
+				typeof (SampleObject),
+				"HelloWorld",
+				WellKnownObjectMode.SingleCall);
+
+			Console.WriteLine("Press the enter key to exit...");
+			Console.ReadLine();
+			return 0;
+		}
+
+	}
+}
